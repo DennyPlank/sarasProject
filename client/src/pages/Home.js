a9b346d0079ce56c095ddf87a71bbc88d57a568a
Denny <denny@Dennys-MacBook-Pro.local>
2022-02-13 10:41:53 -0800

    added cloudinary and some react photo rendering

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,41 @@
 import axios from 'axios';
-import React, { useContext, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Card } from 'react-bootstrap';
 
 const Home =  () => {
+  const [photoSetState, setPhotoSetState] = useState([]);
   
   useEffect(()=>{
     getPhotoSets();
-  });
+  },[]);
 
   const getPhotoSets = async () => {
     let res = await axios.get("/photo_set")
-    console.log(res);
+    setPhotoSetState(res.data);
   }
+  
+  const renderPhotos = () => {
+    console.log("render photos hit")
+    return photoSetState.map((p)=>{
+      console.log(p)
+       return (
+         <div id={p.id}>
+          <Card id="homeCards">
+            <Card.Img src={"https://res.cloudinary.com/doqs3jkhw/image/upload/v1643862183/IMG_5943_s9kc9f.jpg"} alt="Card image" />
+              <Card.ImgOverlay>
+                <Card.Title> {p.name} </Card.Title>
+                <Card.Text> {p.location} </Card.Text>
+            </Card.ImgOverlay>
+          </Card>
+         </div>
+      )
+    })
+  }
+
   return(
     <div>
       <h1>Home</h1>
-     Subset of photos here
+        {renderPhotos()}
     </div>
   )
 };
